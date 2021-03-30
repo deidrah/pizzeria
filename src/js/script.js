71585c69e25c2656dd3276710133985ba3390ffe
Deidrah <dominikastasik93@gmail.com>
2021-03-30 18:05:26 +0200

    fixing indentations

--- src/js/script.js
+++ src/js/script.js
@@ -85,26 +85,24 @@
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
-  initAccordion(){
-    const thisProduct = this;
-    /* find the clickable trigger (the element that should react to clicking) */
-    const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-    /* add event listener to clickable trigger on event click */
-    thisProduct.accordionTrigger.addEventListener('click', function(event) {
+    initAccordion(){
+      const thisProduct = this;
+      /* add event listener to clickable trigger on event click */
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
       /* prevent default action for event */
-      event.preventDefault();
-      /* find active product (product that has active class) */
-      let activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
-      if ((activeProduct != null) && (activeProduct != thisProduct.element)){
-        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
-      }
-      /* toggle active class on thisProduct.element */
-      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
-    });
-   }
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        let activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if ((activeProduct != null) && (activeProduct != thisProduct.element)){
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
+    }
 
-   initOrderForm(){
+    initOrderForm(){
       const thisProduct = this;
       thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
