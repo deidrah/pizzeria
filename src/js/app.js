115db6cef73e94b242d81188c7c27f3852dee750
Deidrah <dominikastasik93@gmail.com>
2021-04-29 07:20:18 +0200

    cleaning up app.js file

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,32 @@
-/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
-
-{
-  'use strict';
-
-  const app = {
-    initCart: function(){
-      const thisApp = this;
-      const cartElem = document.querySelector(select.containerOf.cart);
-      thisApp.cart = new Cart(cartElem);
-    },
-    initMenu: function(){
-      const thisApp = this;
-      for(let productData in thisApp.data.products) {
-        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
-      }
-    },
-    initData: function(){
-      const thisApp = this;
-      thisApp.data = {};
-      const url = settings.db.url + '/' + settings.db.product;
-      fetch(url)
-        .then(function(rawResponse){
-          return rawResponse.json();
-        })
-        .then(function(parsedResponse){
-          thisApp.data.products = parsedResponse;
-          thisApp.initMenu();
-        });
-    },
-    init: function(){
-      const thisApp = this;
-      thisApp.initData();
-      thisApp.initCart();
-    },
-  };
-  app.init();
-}
+const app = {
+  initCart: function(){
+    const thisApp = this;
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+  },
+  initMenu: function(){
+    const thisApp = this;
+    for(let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+  initData: function(){
+    const thisApp = this;
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.product;
+    fetch(url)
+    .then(function(rawResponse){
+      return rawResponse.json();
+    })
+    .then(function(parsedResponse){
+      thisApp.data.products = parsedResponse;
+      thisApp.initMenu();
+    });
+  },
+  init: function(){
+    const thisApp = this;
+    thisApp.initData();
+    thisApp.initCart();
+  },
+};
+app.init();
